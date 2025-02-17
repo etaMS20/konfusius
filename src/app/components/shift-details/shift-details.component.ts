589d10d05721d33bbe34d00bcce55a6629e14b75
MalvSch <malvin.schaedle@etalytics.com>
2025-02-17 22:05:05 +0100

    add checkout details footer

--- src/app/components/shift-details/shift-details.component.ts
+++ src/app/components/shift-details/shift-details.component.ts
@@ -1,16 +1,26 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { ShiftsService } from '../../services/shifts.service';
+import { Observable } from 'rxjs';
+import { WcProduct } from '../shift/shift.model';
+import { CommonModule } from '@angular/common';
+import { MatRadioModule } from '@angular/material/radio';
 
 @Component({
   selector: 'app-shift-details',
   templateUrl: './shift-details.component.html',
   styleUrls: ['./shift-details.component.scss'],
   standalone: true,
+  imports: [CommonModule, MatRadioModule],
 })
-export class ShiftDetailsComponent {
+export class ShiftDetailsComponent implements OnInit {
   private readonly shiftsService = inject(ShiftsService);
+  selectedShift$!: Observable<WcProduct | null>;
 
-  closeFooter() {
+  ngOnInit(): void {
+    this.selectedShift$ = this.shiftsService.getSelectedShift;
+  }
+
+  checkout() {
     // Logic to close the footer, e.g., hide it or emit an event to the parent component
     console.log('Footer closed');
   }
