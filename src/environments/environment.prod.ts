8ae81b405a19c3183f541c2d4e283ee0fa3bb88f
MalvSch <malvin.schaedle@etalytics.com>
2025-02-28 22:05:33 +0100

    remove env

--- src/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: true,
-  backendUrl: 'https://konfusius.org/wp-json',
-  consumerKey: '',
-  consumerSecret: '',
-  password: 'miauindev',
-  salt: 'konfusius',
-};
