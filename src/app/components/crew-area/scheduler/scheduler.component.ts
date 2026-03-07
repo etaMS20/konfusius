3a3ccb634a2c437fd67377eec3299e0bed2a4937
etaMS <malvin.schaedle@etalytics.com>
2026-03-07 16:42:21 +0100

    small cleanup

--- src/app/components/crew-area/scheduler/scheduler.component.ts
+++ src/app/components/crew-area/scheduler/scheduler.component.ts
@@ -29,8 +29,8 @@ import { KTimeUtilsService } from '@services/time-utils.service';
 import { ProgressBarModule } from 'primeng/progressbar';
 import { ToolbarModule } from 'primeng/toolbar';
 import { ButtonModule } from 'primeng/button';
-import jsPDF from 'jspdf';
 import html2canvas from 'html2canvas';
+import { D } from '@angular/common/common_module.d-Qx8B6pmN';
 
 registerLocaleData(localeDe);
 
@@ -167,11 +167,11 @@ export class SchedulerComponent implements OnInit {
     }
   }
 
-  previousDay($event: MouseEvent): void {
+  previousDay(): void {
     this.viewDate.set(subDays(this.viewDate(), 1));
   }
 
-  nextDay($event: MouseEvent): void {
+  nextDay(): void {
     this.viewDate.set(addDays(this.viewDate(), 1));
   }
 
@@ -181,7 +181,7 @@ export class SchedulerComponent implements OnInit {
 
     for (let i = 0; i < 3; i++) {
       this.viewDate.set(addDays(originalDate, i));
-      await new Promise((resolve) => setTimeout(resolve, 300));
+      await new Promise((resolve) => setTimeout(resolve, 200));
       const el = document.querySelector('mwl-calendar-week-view')!;
       canvases.push(await html2canvas(el as HTMLElement));
     }
@@ -201,7 +201,7 @@ export class SchedulerComponent implements OnInit {
 
     const link = document.createElement('a');
     link.href = merged.toDataURL('image/png');
-    link.download = 'calendar.png';
+    link.download = new Date().toLocaleDateString('de-DE') + '-schedule.png';
     link.click();
   }
 }
